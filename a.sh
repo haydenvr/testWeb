03e6591ca0b6de4e1c0384802cf67db34e40af6f
Hayden van Reyswoud <h.v.reyswoud@gmail.com>
2016-07-20 10:46:48 +1000

    got template

---
+++ a.sh
@@ -0,0 +1,12 @@
+bin/pip install flask
+bin/pip install flask-login
+bin/pip install flask-openid
+bin/pip install flask-mail
+bin/pip install flask-sqlalchemy
+bin/pip install sqlalchemy-migrate
+bin/pip install flask-whooshalchemy
+bin/pip install flask-wtf
+bin/pip install flask-babel
+bin/pip install guess_language
+bin/pip install flipflop
+bin/pip install coverage
